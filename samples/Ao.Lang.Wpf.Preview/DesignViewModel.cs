8f6b547bbec846d886e74b7e627bd483e96f25aa
HCricle <15218450198@163.com>
2022-10-27 11:17:47 +0800

    Sample for #2

---
+++ samples/Ao.Lang.Wpf.Preview/DesignViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Ao.Lang.Runtime;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ao.Lang.Wpf.Preview
+{
+    public class DesignViewModel
+    {
+        public DesignViewModel()
+        {
+            LangLoader.Load();
+        }
+    }
+}
---
+++ samples/Ao.Lang.Wpf.Preview/LangLoader.cs
@@ -0,0 +1,28 @@
+﻿using Ao.Lang.Runtime;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Ao.Lang.Wpf.Preview
+{
+    internal class LangLoader
+    {
+        public static void Load()
+        {
+            LanguageManager.Instance.SetCulture("zh-CN");
+            var ser = LanguageManager.Instance.LangService;
+            var zhCNNode = ser.EnsureGetLangNode("zh-CN");
+            zhCNNode.AddJsonFile(GetStringFile("zh_CN", "main.json"));
+            var asm = typeof(LangLoader).Assembly;
+            zhCNNode.AddResourceStream(typeof(LangLoader).Assembly.GetManifestResourceStream("Ao.Lang.Wpf.Preview.Strings.zh_CN.main.resources"));
+            var enUSNode = ser.EnsureGetLangNode("en-US");
+            enUSNode.AddJsonFile(GetStringFile("en_US", "main.json"));
+        }
+
+        private static string GetStringFile(params string[] nodes)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Strings");
+            return Path.Combine(path, Path.Combine(nodes));
+        }
+    }
+}
