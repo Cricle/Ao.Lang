965a64867d5d2805909d7d385c7f74e82c6a03fc
flier268 <flier268@gmail.com>
2023-01-07 14:28:00 +0800

    fix: STA Thread in unit test

--- test/Ao.Lang.Wpf.Test/LangExtensionsTest.cs
+++ test/Ao.Lang.Wpf.Test/LangExtensionsTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -17,32 +18,37 @@ namespace Ao.Lang.Wpf.Test
     public class LangExtensionsTest
     {
         [TestMethod]
-        [STAThread]
         public void GivenNullCall_MustThrowException()
         {
-            var tbx = new TextBlock();
-            var run = new Run();
-            var dp = TextBlock.TextProperty;
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang((FrameworkElement)null, dp, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang((FrameworkContentElement)null, dp, "a"));
+            Thread t = new Thread(new ThreadStart(delegate ()
+            {
+                var tbx = new TextBlock();
+                var run = new Run();
+                var dp = TextBlock.TextProperty;
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang((FrameworkElement)null, dp, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang((FrameworkContentElement)null, dp, "a"));
 
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, dp, null));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, dp, null));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, dp, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, dp, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, "a"));
 
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, dp, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, dp, null));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, dp, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, dp, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, dp, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, dp, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(tbx, null, dp, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindLang(run, null, dp, "a"));
 
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText((TextBlock)null, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText((Run)null, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText(tbx, null));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText(run, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText((TextBlock)null, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText((Run)null, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText(tbx, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.BindText(run, null));
 
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.CreateLangBinding(null, "a"));
-            Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.CreateLangBinding(LanguageManager.Instance, null));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.CreateLangBinding(null, "a"));
+                Assert.ThrowsException<ArgumentNullException>(() => LangExtensions.CreateLangBinding(LanguageManager.Instance, null));
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
         }
         [TestMethod]
         [STAThread]
@@ -52,45 +58,54 @@ namespace Ao.Lang.Wpf.Test
             Assert.IsNotNull(bind);
         }
         [TestMethod]
-        [STAThread]
         public void BindText_MustBeBinded()
         {
-            var tbx = new TextBlock();
-            var run = new Run();
-            LangExtensions.BindText(tbx, "hello");
-            var tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
-            Assert.IsNotNull(tbxBind);
-            LangExtensions.BindText(run, "hello");
-            var runBind = run.GetBindingExpression(Run.TextProperty);
-            Assert.IsNotNull(runBind);
+            Thread t = new Thread(new ThreadStart(delegate ()
+            {
+                var tbx = new TextBlock();
+                var run = new Run();
+                LangExtensions.BindText(tbx, "hello");
+                var tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
+                Assert.IsNotNull(tbxBind);
+                LangExtensions.BindText(run, "hello");
+                var runBind = run.GetBindingExpression(Run.TextProperty);
+                Assert.IsNotNull(runBind);
 
-            LangExtensions.BindText(tbx, new LanguageManager(), "hello");
-            tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
-            Assert.IsNotNull(tbxBind);
-            LangExtensions.BindText(run, new LanguageManager(), "hello");
-            runBind = run.GetBindingExpression(Run.TextProperty);
-            Assert.IsNotNull(runBind);
-
+                LangExtensions.BindText(tbx, new LanguageManager(), "hello");
+                tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
+                Assert.IsNotNull(tbxBind);
+                LangExtensions.BindText(run, new LanguageManager(), "hello");
+                runBind = run.GetBindingExpression(Run.TextProperty);
+                Assert.IsNotNull(runBind);
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
         }
         [TestMethod]
-        [STAThread]
         public void BindProperty_MustBeBinded()
         {
-            var tbx = new TextBlock();
-            var run = new Run();
-            LangExtensions.BindLang(tbx, TextBlock.TextProperty, "hello");
-            var tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
-            Assert.IsNotNull(tbxBind);
-            LangExtensions.BindLang(run, Run.TextProperty, "hello");
-            var runBind = run.GetBindingExpression(Run.TextProperty);
-            Assert.IsNotNull(runBind);
+            Thread t = new Thread(new ThreadStart(delegate ()
+            {
+                var tbx = new TextBlock();
+                var run = new Run();
+                LangExtensions.BindLang(tbx, TextBlock.TextProperty, "hello");
+                var tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
+                Assert.IsNotNull(tbxBind);
+                LangExtensions.BindLang(run, Run.TextProperty, "hello");
+                var runBind = run.GetBindingExpression(Run.TextProperty);
+                Assert.IsNotNull(runBind);
 
-            LangExtensions.BindLang(tbx, new LanguageManager(), TextBlock.TextProperty, "hello");
-            tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
-            Assert.IsNotNull(tbxBind);
-            LangExtensions.BindLang(run, new LanguageManager(), Run.TextProperty, "hello");
-            runBind = run.GetBindingExpression(Run.TextProperty);
-            Assert.IsNotNull(runBind);
+                LangExtensions.BindLang(tbx, new LanguageManager(), TextBlock.TextProperty, "hello");
+                tbxBind = tbx.GetBindingExpression(TextBlock.TextProperty);
+                Assert.IsNotNull(tbxBind);
+                LangExtensions.BindLang(run, new LanguageManager(), Run.TextProperty, "hello");
+                runBind = run.GetBindingExpression(Run.TextProperty);
+                Assert.IsNotNull(runBind);
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
         }
     }
 }
--- test/Ao.Lang.Wpf.Test/LangTest.cs
+++ test/Ao.Lang.Wpf.Test/LangTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Markup;
@@ -13,14 +14,15 @@ namespace Ao.Lang.Wpf.Test
         [TestMethod]
         public void InitUsingConstruct_MustInitProperty()
         {
-            var l = new CLang();
-            Assert.IsNull(l.Key);
-            Assert.AreEqual(0, l.Args.Capacity);
-
-
-            l = new CLang("hello");
-            Assert.AreEqual("hello", l.Key);
-
+            Thread t = new Thread(new ThreadStart(delegate ()
+            {
+                var l = new CLang();
+                Assert.IsNull(l.Key);
+                Assert.AreEqual(0, l.Args.Capacity);
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
         }
         class ValueProvideValueTarget : IProvideValueTarget
         {
@@ -43,17 +45,23 @@ namespace Ao.Lang.Wpf.Test
         [DataRow(true)]
         public void Bind_MustReturnBindingExpression(bool noUpdate)
         {
-            var lang = new CLang("title");
-            lang.NoUpdate = noUpdate;
-            var tbx = new TextBlock();
-            var target = new ValueProvideValueTarget
+            Thread t = new Thread(new ThreadStart(delegate ()
             {
-                TargetObject = tbx,
-                TargetProperty = TextBlock.TextProperty
-            };
-            var provider = new ValueServiceProvider { Value = target };
-            var obj = lang.ProvideValue(provider);
-            Assert.IsInstanceOfType(obj, typeof(BindingExpression));
+                var lang = new CLang("title");
+                lang.NoUpdate = noUpdate;
+                var tbx = new TextBlock();
+                var target = new ValueProvideValueTarget
+                {
+                    TargetObject = tbx,
+                    TargetProperty = TextBlock.TextProperty
+                };
+                var provider = new ValueServiceProvider { Value = target };
+                var obj = lang.ProvideValue(provider);
+                Assert.IsInstanceOfType(obj, typeof(BindingExpression));
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
         }
 #endif
     }
