ecde2bb09a8e8ea8fd245fa00cda0b25bc15005f
HCricle <15218450198@163.com>
2023-08-14 17:42:15 +0800

    Remove note

--- src/Ao.Lang/LanguageBuilder.cs
+++ src/Ao.Lang/LanguageBuilder.cs
@@ -24,14 +24,6 @@ namespace Ao.Lang
         {
             Debug.Assert(Culture != null);
             var providers = Sources.Select(x => x.Build(this)).ToArray();
-
-            //var sourceCount = Sources.Count();
-            //var providers = new IConfigurationProvider[sourceCount];
-            //for (int i = 0; i < sourceCount; i++)
-            //{
-            //    providers[i] = Sources[i].Build(this);
-            //}
-
             return new LanguageRoot(Culture, providers);
         }
     }
