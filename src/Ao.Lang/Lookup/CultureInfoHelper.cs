f2f06460763f8a67245aa78964f1ce91b4f6d2cc
HCricle <15218450198@163.com>
2023-08-14 17:16:28 +0800

    The TFM is upper .NETSTANDARD2.0, I don't want to use `IsAvaliableCulture` use throw to check.
    So I almost reset the changes

--- src/Ao.Lang/Lookup/CultureInfoHelper.cs
+++ src/Ao.Lang/Lookup/CultureInfoHelper.cs
@@ -1,25 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Ao.Lang.Lookup
 {
     public static class CultureInfoHelper
     {
+        private static readonly HashSet<string> avaliableCultures = new HashSet<string>(
+            CultureInfo.GetCultures(CultureTypes.AllCultures).Select(x => x.Name),
+            StringComparer.OrdinalIgnoreCase);
         public static bool IsAvaliableCulture(string culture)
         {
             if (string.IsNullOrEmpty(culture))
             {
                 throw new ArgumentException($"“{nameof(culture)}”不能为 null 或空。", nameof(culture));
             }
-            try
-            {
-                // Todo: GetCultureInfoByIetfLanguageTag was Deprecated
-                return (CultureInfo.GetCultureInfoByIetfLanguageTag(culture).CultureTypes & CultureTypes.UserCustomCulture) != CultureTypes.UserCustomCulture;
-            }
-            catch
-            {
-                return false;
-            }
+            return avaliableCultures.Contains(culture);
         }
     }
 }
